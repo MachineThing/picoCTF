df6618574fff81752a257a8ba4494cdf12311d05
Mason Fisher <sackzombie10@gmail.com>
2020-09-08 10:40:29 -0400

    Added comments

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -1,18 +1,18 @@
-from string import ascii_lowercase
+from string import ascii_lowercase              # Lowercase ASCII characters
 
-cipher = tuple("odaeeuzsftqdgnuoazxvymiumq")
-cipher_copy = None
-alpha = tuple(ascii_lowercase)
-letter_index = 0
+cipher = tuple("odaeeuzsftqdgnuoazxvymiumq")    # The cipher
+cipher_copy = None                              # This variable holds a copy of the cipher
+alpha = tuple(ascii_lowercase)                  # Lowercase ASCII characters
+letter_offset = 0
 
-for shift in range(26):
+for shift in range(26): # Repeat this 26 times
     cipher_copy = list(cipher)
     cipher_index = 0
     for letter in cipher:
-        letter_ord = ord(letter) + letter_index
-        if letter_ord > ord('z'):
-            letter_ord = letter_ord - 26
+        letter_ord = ord(letter) + letter_offset # Set "letter_ord" to the ASCII value of the letter plus offset
+        if letter_ord > ord('z'): # If the "letter_ord" variable is greater than the ASCII value of the lowercase letter z
+            letter_ord = letter_ord - 26 # Subtract 26 from "letter_ord"
         cipher_copy[cipher_index] = chr(letter_ord)
         cipher_index = cipher_index + 1
-    letter_index = letter_index + 1
-    print(str(letter_index)+":\tpicoCTF{"+''.join(cipher_copy)+"}")
+    letter_offset = letter_offset + 1
+    print(str(letter_offset)+":\tpicoCTF{"+''.join(cipher_copy)+"}") # Prints "offset: [tab] picoCTF{cracked_cipher}"
--- the_numbers/numbers.py
+++ the_numbers/numbers.py
@@ -1,15 +1,13 @@
-numbers = (16, 9, 3, 15, 3, 20, 6)
-numbers_two = (20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14)
+numbers = (16, 9, 3, 15, 3, 20, 6) # picoctf
+numbers_two = (20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14) # thenumbersmason
 
 numbers_crack = []
 numbers_crack_two = []
 
 for i in numbers:
-    numbers_crack.append(chr(i+96))
-
-print(numbers_crack)
+    numbers_crack.append(chr(i+96)) # append the ASCII character of the number plus 96
 
 for i in numbers_two:
-    numbers_crack_two.append(chr(i+96))
+    numbers_crack_two.append(chr(i+96)) # like line 8, append the ASCII character of the number plus 96
 
-print(numbers_crack_two)
+print(numbers_crack, numbers_crack_two)
